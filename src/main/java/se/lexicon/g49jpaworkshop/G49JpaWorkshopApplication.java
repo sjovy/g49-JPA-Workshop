b15c30fa4b1179010bc7f716c29e1dbb0ae97034
binleiden <daniel.petersson83@gmail.com>
2024-05-29 10:01:32 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g49jpaworkshop/G49JpaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G49JpaWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G49JpaWorkshopApplication.class, args);
+    }
+
+}
