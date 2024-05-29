ad4741c7b4218ffdcce1a5513d3d123a0d8628d6
binleiden <daniel.petersson83@gmail.com>
2024-05-29 10:24:03 +0200

    Added new entity classes: AppUser and Details

---
+++ src/main/java/se/lexicon/g49jpaworkshop/entity/AppUser.java
@@ -0,0 +1,12 @@
+package se.lexicon.g49jpaworkshop.entity;
+
+import java.time.LocalDate;
+
+public class AppUser {
+    private int id;
+    private String userName;
+    private String password;
+    private LocalDate regDate;
+    private Details userDetails;
+
+}
---
+++ src/main/java/se/lexicon/g49jpaworkshop/entity/Details.java
@@ -0,0 +1,10 @@
+package se.lexicon.g49jpaworkshop.entity;
+
+import java.time.LocalDate;
+
+public class Details {
+    private int id;
+    private String email;
+    private String name;
+    private LocalDate birthDay;
+}
