c680e6ac21b6fa17a9308ee56f207a7b0c72cd32
binleiden <daniel.petersson83@gmail.com>
2024-05-29 10:52:05 +0200

    Established OneToOne relationship between AppUser and Details entities

--- src/main/java/se/lexicon/g49jpaworkshop/entity/AppUser.java
+++ src/main/java/se/lexicon/g49jpaworkshop/entity/AppUser.java
@@ -1,12 +1,31 @@
 package se.lexicon.g49jpaworkshop.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Entity
+@Data
+
 public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false, unique = true)
     private String userName;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column
     private LocalDate regDate;
+
+    @Column
+    @OneToOne
+    @JoinColumn(name = "details_id")
     private Details userDetails;
 
 }
--- src/main/java/se/lexicon/g49jpaworkshop/entity/Details.java
+++ src/main/java/se/lexicon/g49jpaworkshop/entity/Details.java
@@ -1,10 +1,21 @@
 package se.lexicon.g49jpaworkshop.entity;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+@Entity
 
 public class Details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Column
     private LocalDate birthDay;
 }
