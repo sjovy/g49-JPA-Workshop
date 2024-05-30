dc64ca12aaa885d1bc4c6f10167636c2981c6424
binleiden <daniel.petersson83@gmail.com>
2024-05-30 12:21:21 +0200

    Added constructors

--- src/main/java/se/lexicon/g49jpaworkshop/entity/Details.java
+++ src/main/java/se/lexicon/g49jpaworkshop/entity/Details.java
@@ -1,21 +1,31 @@
 package se.lexicon.g49jpaworkshop.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Details {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false, unique = true)
+    private String name;
 
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false, unique = true)
-    private String name;
-
     @Column
     private LocalDate birthDay;
+
+    public Details(String name, String email, LocalDate birthDay) {
+        this.name = name;
+        this.email = email;
+        this.birthDay = birthDay;
+    }
 }
